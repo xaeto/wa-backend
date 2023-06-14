a8cd8a987061135a01b9f5a88e6b1aaa5b6c4a58
Jannik Brozy <jannik.brozy@student.fh-kiel.de>
2023-06-14 17:10:47 +0200

    init

---
+++ src/db.js
@@ -0,0 +1,19 @@
+var sqlite3 = require("sqlite3").verbose();
+const DBSOURCE = "db.sqlite";
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+    if(err){
+        console.error(err.message);
+        throw err;
+    } else {
+        db.run(`
+            CREATE TABLE IF NOT EXISTS Leaderboard(
+                name varchar(32) not null,
+                score int not null,
+                created TEXT DEFAULT CURRENT_TIMESTAMP
+            )
+        `)
+    }
+});
+
+module.exports = db;
---
+++ src/server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const db = require('./db.js')
+
+var bodyParser = require('body-parser');
+var cors = require('cors')
+
+const app = express();
+app.use(cors())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+const port = 3000;
+
+app.get("/leaderboard", (req, res) => {
+  const sql = "select * from leaderboard";
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if(err) {
+      res.status(400).json({"error": err.message});
+      return;
+    }
+    res.json(rows)
+  });
+});
+
+app.post("/leaderboard/createentry", (req, res) => {
+  const name = req.body["username"];
+  const score = req.body["score"];
+  const sql = `
+    INSERT INTO leaderboard (name, score) VALUES ("${name}", ${score})
+  `;
+  const params = [];
+  db.run(sql, params, (err, rows) => {
+    if(err) {
+      res.status(400).json({"error": err.message});
+      return;
+    }
+    res.status(201);
+    res.send("test")
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`listening on ${port}`)
+});
